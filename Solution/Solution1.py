6302246a877927c6fb57b5bdf163762f738cf408
21bca54 <96609681+21bca54@users.noreply.github.com>
2022-03-07 20:00:33 +0530

    Add files via upload

---
+++ Solution/Solution1.py
@@ -0,0 +1,8 @@
+#Write a python script to enter 2 number and print the sum of it.
+a=int(input("Enter any num1:"))
+b=int(input("Enter any num2:"))
+c=0
+print(a)
+print(b)
+c=a+b
+print("Sum of {} and {} is:{} ".format(a,b,c))
