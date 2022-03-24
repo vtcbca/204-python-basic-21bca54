73ea8701478e878a4548be3b5d9023a3e314e52c
21bca54 <96609681+21bca54@users.noreply.github.com>
2022-03-24 11:17:41 +0530

    Update Solution 10.py
    I can  erase the non usable  variable su in my program

--- Solution 10.py
+++ Solution 10.py
@@ -3,11 +3,9 @@ no=int(input("Enter any num:"))
 o=no
 e=0
 re=0
-su=0
 while no!=0:
       re=no%10
-      e=e*10+re
-      
+      e=e*10+re  
       no=no//10
 if(o==e):
     print("The {} is  Palindrome.".format(o))
