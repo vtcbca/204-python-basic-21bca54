57fd313e3c7e1bb792ed69e04700f793a025a81a
21bca54 <96609681+21bca54@users.noreply.github.com>
2022-04-22 10:39:36 +0530

    Update Solution 6.py

--- Solution 6.py
+++ Solution 6.py
@@ -4,12 +4,12 @@ if(a==1):
 elif(a==2):
     print("Tuesday")
 elif(a==3):
-    print("Wednsday")
+    print("Wednesday")
 elif(a==4):
-    print("Thershday")
+    print("Thursday")
 elif(a==5):
     print("Friday")
 elif(a==6):
-    print("Saterday")
+    print("Saturday")
 elif(a==7):
     print("Sunday")
